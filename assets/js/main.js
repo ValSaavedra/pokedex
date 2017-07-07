37f3afa8376689fdf8a782d35e2cde24f8fded13
ValSaavedra <val.dicion@gmail.com>
2017-07-07 13:54:53 -0400

    js y html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-main.js
+$(document).ready(function($){
+	$.ajax({
+		url:'http://pokeapi.co/api/v2/pokemon/',
+		type: 'GET',
+		datatype: 'JSON',
+		data:("limit": '811'),
+	})
+	.done(function(respuesta) {
+		console.log(respuesta);
+		console.log("Success");
+	})
+	.fail(function)() {
+		console.log("error");
+	})
+	.always(function) {
+		console.log("complete");
+	});
+});
