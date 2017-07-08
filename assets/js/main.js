3c69222e18e0dac4c1ac8abec22ce56dc7156d5f
ValSaavedra <val.dicion@gmail.com>
2017-07-07 21:47:34 -0400

    html y js falta css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
-$(document).ready(function($){
+$(document).ready(function($){	
 	$.ajax({
 		url:'http://pokeapi.co/api/v2/pokemon/',
 		type: 'GET',
 		datatype: 'JSON',
-		data:("limit": '811'),
+		data:{"limit": '811'},
 	})
 	.done(function(respuesta) {
-		console.log(respuesta);
+		//llamar pokemon container con jquery
+		respuesta.results.forEach(function(e, img){
+		var fotoPoke = img + 1;
+
+		$(".pokemon").append("<div class='col s4'><img src='http://pokeapi.co/media/img/" + fotoPoke + ".png'><div>" + e.name + "</div></div>")
+		})
+		
 		console.log("Success");
 	})
-	.fail(function)() {
+	.fail(function(){
 		console.log("error");
 	})
-	.always(function) {
+	.always(function() {
 		console.log("complete");
 	});
 });
+
